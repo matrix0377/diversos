0d6624a3e1697091f5f059f776de4ff5eb55f337
Davi Augusto <droidcenter0377@gmail.com>
2022-04-05 15:38:11 -0300

    Aprimoramento

--- EVGclasseLibraryBook.py
+++ EVGclasseLibraryBook.py
@@ -6,36 +6,65 @@ class LibraryBook (object):
       self.call_number = call_no
       self.checked_out = False
 
-      my_book = LibraryBook()
-
-      my_book.title = "Harry Potter and the Philosopher's Stone"
-      my_book.author = ('Rowling', 'J.K.')
-      my_book.year = 1998
-      my_book.call_number = "PZ7.R79835"
-
-      # new_book = LibraryBook("Harry Potter and the Sorcerer's Stone", 
-      #                  ("Rowling","J.K."), 1998, "PZ7.R79835")
-
       
        # Retorna o título e as informações do autor do livro como uma string
     def title_and_author(self):
-            return "{self.author[0]}: {self.title[0]}" 
+        return (f"Autor: {self.author}    : Título : {self.title}") 
+        
         
         # Imprime todas as informações associadas a um livro neste formato
-    def __str__(self): # certifique-se de que __str__ retorna uma string!
-            return "{} {} ({}): {}".format(self.author[1], self.author[0], self.year, self.title)
+        # certifique-se de que __str__ retorna uma string!
+    def __str__(self): 
+        return "{} --->> {} ({}): {}".format(self.call_number, self.author, self.year, self.title)
+
 
         # Retorna uma representação de string do livro com o título e call_number     
     def __repr__(self): 
-            return "<Book: {} ({})>".format(self.title, self.call_number)
-            print(my_book)  
-            my_book.title_and_author()
+        return ("Book     : {},\nCall_Number: {}".format(self.title, self.call_number))
+        
             
             
+my_book=LibraryBook(
+    "Harry Potter and the Philosopher's Stone",
+    "Rowling, J.K",
+    1998,
+    "PZ7.R79835"
+    )
 
-print(LibraryBook)
-print(LibraryBook.title_and_author)
-print(LibraryBook.__repr__)
+my_book2 = LibraryBook(
+    "Learning Perl",
+    "Schuwartz, Randal L.",
+    2008,
+    "QA76.73.P22"
+)
 
 
-              
+# my_book.title = "Harry Potter and the Philosopher's Stone"
+# my_book.author = ('Rowling', 'J.K.')
+# my_book.year = 1998
+# my_book.call_number = "PZ7.R79835"
+
+# new_book = LibraryBook("Harry Potter and the Sorcerer's Stone",
+#                  ("Rowling","J.K."), 1998, "PZ7.R79835")
+
+# my_book1 e my_book2
+print('\n')
+
+print('-=' * 35, "\n")
+print('----- Função title_and_author  ------\n')
+print(my_book.title_and_author())
+print(my_book2.title_and_author())
+print('\n')
+
+print('-=' * 35, "\n")
+print('----- Função __repr___  ------\n')
+print(my_book.__repr__())
+print('\n')
+print(my_book2.__repr__())
+
+print('-=' * 35, "\n")
+print('----- Função __str__  ------\n')
+print(my_book.__str__())
+print('\n')
+print(my_book2.__str__())
+print('\n')
