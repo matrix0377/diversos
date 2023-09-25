73726fe3df8410bd4d49204c87136ae138b380b3
Davi Augusto <davi_cianorte@hotmail.com>
2023-09-25 20:11:25 -0300

    acrescentando testes

---
+++ posicao_mouse/teste_horas.py
@@ -0,0 +1,39 @@
+from datetime import datetime
+
+
+now = datetime.now()
+d = now.strftime("%m/%d/%Y - %H:%M:%S")
+print("Output 2:", d)
+
+#print("Date time object:", now)
+
+	
+
+# d = date_time.strftime("%d %b, %Y")
+# print("Output 3:", d)
+
+# d = date_time.strftime("%d %B, %Y")
+# print("Output 4:", d)
+
+# d = date_time.strftime("%I%p")
+# print("Output 5:", d)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# date_time = now.strftime("%d/%m/%Y, %H:%M")
